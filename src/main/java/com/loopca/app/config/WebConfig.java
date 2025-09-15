318c963b37959d8eea08e122135db25798fcb473
hseung127 <hseung127@gmail.com>
2025-09-15 20:22:08 +0900

    cors 수정

--- src/main/java/com/loopca/app/config/WebConfig.java
+++ src/main/java/com/loopca/app/config/WebConfig.java
@@ -5,18 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Value("${cors.allowed-origins}") // application.yml에서 불러오기
-    private List<String> allowedOrigins;
+    private String allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로 허용
-                .allowedOrigins(allowedOrigins.toArray(new String[0])) // 특정 도메인만 허용
+                .allowedOrigins(allowedOrigins.split(",")) // 특정 도메인만 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
